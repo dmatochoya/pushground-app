6598466dfe76e964d448d81a426687728a845bd0
David Mato <dmatochoya@gmail.com>
2022-08-12 00:25:02 +0200

    Add cliploader to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { useAppSelector } from './app/hooks';
+import {
+  BrowserRouter as Router, Routes, Route, Navigate,
+} from 'react-router-dom';
+import ClipLoader from 'react-spinners/ClipLoader';
+import GlobalStyles from './theme/GlobalStyles';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import { ClipLoaderContainer } from './components/styles/ClipLoaderContainer.styled';
+import { useAppSelector } from './app/hooks';
+import { useGetApiKey } from './hooks/useGetApiKey';
 
 function App() {
   const isUserAuthenticated = useAppSelector(
@@ -10,13 +16,28 @@ function App() {
   );
 
   const currentRoute = isUserAuthenticated ? '/dashboard' : '/login';
+  const isLoading = useGetApiKey(isUserAuthenticated);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to={currentRoute} replace />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+    <>
+      <GlobalStyles />
+      {isLoading
+        ? (
+          <ClipLoaderContainer>
+            <ClipLoader color="#ff0000" size={100} />
+          </ClipLoaderContainer>
+        )
+        : (
+          <Router>
+            <Routes>
+              <Route path="*" element={<Navigate to={currentRoute} replace />} />
+              {!isUserAuthenticated
+                ? <Route path="/login" element={<Login />} />
+                : <Route path="/dashboard" element={<Dashboard />} />}
+            </Routes>
+          </Router>
+        )}
+    </>
   );
 }
 
