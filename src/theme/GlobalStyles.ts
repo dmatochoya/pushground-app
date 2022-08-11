badc0658526af76b20d672649ebc538747169b4f
David Mato <dmatochoya@gmail.com>
2022-08-12 00:23:02 +0200

    Add global styles

---
+++ src/theme/GlobalStyles.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    * {
+      box-sizing: border-box;
+      margin: 0;
+    }
+
+    body {
+      font-family: Poppins, sans-serif;
+    }
+
+    input {
+      outline: none;
+    }
+
+    button {
+      background-color: transparent;
+      border: none;
+      cursor: pointer;
+      -webkit-tap-highlight-color: transparent;
+    }
+`;
+
+export default GlobalStyles;
