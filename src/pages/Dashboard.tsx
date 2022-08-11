87ebb35879d4b9bcaa547d4efdd4a50ecb2c3b75
David Mato <dmatochoya@gmail.com>
2022-08-12 00:25:39 +0200

    Add Dashboard component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from '../components/Header';
+import Filters from '../components/Filters';
+import Table from '../components/Table';
+import { StyledMain } from '../components/styles/DashboardHeaderAndMain.styled';
 
 function Dashboard() {
-  return <h1>Bye</h1>;
+  const dateObj = new Date();
+  const dateOneWeekAgo = new Date(dateObj.setDate(dateObj.getDate() - 7));
+  const [startDate, setStartDate] = useState(dateOneWeekAgo);
+  const [endDate, setEndDate] = useState(new Date());
+
+  const dates = {
+    startDate, setStartDate, endDate, setEndDate,
+  };
+
+  return (
+    <section>
+      <Header />
+      <StyledMain>
+        <Filters dates={dates} />
+        <Table startDate={startDate} endDate={endDate} />
+      </StyledMain>
+    </section>
+  );
 }
 
 export default Dashboard;
