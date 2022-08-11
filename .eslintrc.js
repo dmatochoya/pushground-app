c4fedd05f59138354cbd4c9a0d6775efcb952287
David Mato <dmatochoya@gmail.com>
2022-08-12 00:21:06 +0200

    Add eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,9 +23,12 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
     'import/extensions': ['error', 'ignorePackages', { ts: 'never', tsx: 'never' }],
-    'no-shadow': ['error', { allow: ['isUserAuthenticated'] }],
+    'no-shadow': ['error', { allow: ['isUserAuthenticated', 'userName', 'apiKey'] }],
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['state'] }],
     'import/prefer-default-export': 0,
     'no-unused-vars': 0,
+    'no-nested-ternary': 0,
+    'react/no-array-index-key': 0,
   },
   settings: {
     'import/resolver': {
